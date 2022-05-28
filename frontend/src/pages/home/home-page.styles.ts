728157e854fb519b9a27fb49e08dfdfce6abd07f
develtype <develtype@gmail.com>
2022-05-28 13:36:20 +0900

    feat: temporary home content

--- frontend/src/pages/home/home-page.styles.ts
+++ frontend/src/pages/home/home-page.styles.ts
@@ -4,6 +4,29 @@ const root = css({
   width: '100%',
 });
 
+const content = css({
+  width: '100%',
+  padding: '20px 30px',
+});
+
+const contentTitle = css({
+  color: '#777777',
+  fontSize: 20,
+  fontWeight: 600,
+});
+
+const list = css({
+  paddingLeft: 10,
+});
+
+const item = css({
+  color: '#666666',
+});
+
 export default {
   root,
+  content,
+  contentTitle,
+  list,
+  item,
 };
