6aef03bd431abc3408fb170ec3e322eef776c3dc
develtype <develtype@gmail.com>
2022-05-28 14:44:10 +0900

    refactor: init users data refactor

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,18 +1,26 @@
+import { util } from '../util/util';
+
 export type UserType = {
   id: number;
   name: string;
   email: string;
 }
 
-const users: UserType[] = [
-  {
-    id: 1,
-    name: 'test',
-    email: 'test@gmail.com',
+const usersData: { [id: string]: UserType; } = Array.from(Array(10).keys()).reduce<{ [id: string]: UserType; }>(
+  (res, item) => {
+    const name = util.generateRandomString();
+    const userItem = {
+      id: item + 1,
+      name,
+      email: name + '@gmail.com',
+    };
+    res[item] = userItem;
+    return res;
   },
-];
+  {},
+);
 
-const fetchUsers = async () => users;
+const fetchUsers = async () => Object.values(usersData);
 
 export const usersService = {
   fetchUsers,
